f6110943c23893112a1cb3dae13e6de814b2e778
Raiicon <orankai@gmail.com>
2018-11-23 15:14:54 -0500

    cleaned up the game.js code and added more comments

--- javascript/game.js
+++ javascript/game.js
@@ -11,13 +11,14 @@ window.addEventListener("DOMContentLoaded", function() {
         // creates a box object
         var box = BABYLON.Mesh.CreateBox("Box",4,0,scene);
 
-
+        // creates a camera and attaches it to the scene. an "arc" camera will rotate around a target in an arc
         var camera =  new BABYLON.ArcRotateCamera("arcCamera",
         BABYLON.Tools.ToRadians(45),
         BABYLON.Tools.ToRadians(45),
         10.0, box.position,scene);
+        // attaches the camera to the canvas
         camera.attachControl(canvas,true);
-
+        // Create a light, and add a color to it
         var light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0,10,0), scene);
         light.diffuse = new BABYLON.Color3(1,0,0);
 
@@ -29,18 +30,3 @@ window.addEventListener("DOMContentLoaded", function() {
         scene.render();
     });
 });
-
-
-// var scene = new BABYLON.Scene(engine);
-
-// Creates and positions a free camera
-// var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-// targets the camera to scene origin
-
-// attaches the camera to the canvas
-// camera.attachControl(canvas, true);
-
-// Creates a light
-// var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0,1,0), scene);
-
