88b0567d535f56f8f3f6cd66fa9e251f8927372d
Raiicon <orankai@gmail.com>
2018-11-23 16:02:28 -0500

    experimenting with adding objects and light elemements.

--- javascript/game.js
+++ javascript/game.js
@@ -19,14 +19,30 @@ window.addEventListener("DOMContentLoaded", function() {
         // attaches the camera to the canvas
         camera.attachControl(canvas,true);
         // Create a light, and add a color to it
-        var light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0,10,0), scene);
+        var light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0,10,7), scene);
         light.diffuse = new BABYLON.Color3(1,0,0);
+        // Create a built-in "sphere" shape. 
+        var sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {segments:16, diameter:2}, scene);
+        sphere.position.y = 1;
+        sphere.position.x = -7;
+        // Create a built-in "ground" shape.
+        var ground = BABYLON.MeshBuilder.CreateGround('ground1', {height:6, width:6, subdivisions: 2}, scene);
+        ground.position.y = -2;
+
+        var light2 = new BABYLON.HemisphericLight('light2', new BABYLON.Vector3(0,1,-2), scene);
+        light.diffuse = new BABYLON.Color3(.8,0,.8);
 
         return scene;
     }
-
+    // This is calling the scene we created
     var scene = createScene();
+    // will run code in here once per refresh, as fast as computer can go
     engine.runRenderLoop( function () {
         scene.render();
     });
+    // this will handle resizing the canvas/window
+    window.addEventListener('resize', function() {
+        engine.resize();
+    });
+    
 });
